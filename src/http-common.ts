3732cf55a310e8092bbdd577194608bd7334033a
ihubregion7 <ihubregion7@gmail.com>
2023-08-07 22:15:03 +0800

    Updated client module

--- src/http-common.ts
+++ src/http-common.ts
@@ -5,6 +5,9 @@ const apiClient: AxiosInstance = axios.create({
         username: 'admin',
         password: 'qwerty'
     },
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    },
     baseURL:'https://cfidp.cebudigital.com/app',
     // baseURL:'http://localhost:8000/',
     // baseURL:'https://cfidp.cebudigital.com/api',
